ffa6111044750a2f3a23270868d4e8ecefc43dfc
Arleny <arleny.ares@gmail.com>
2024-12-22 16:40:40 +0100

    first commit

---
+++ src/test/java/arlenyAres/FutCat_Api/FutCatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package arlenyAres.FutCat_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutCatApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
