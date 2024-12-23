a69090617cecee918b6ddaa892cb44b9e885f56b
Arleny <arleny.ares@gmail.com>
2024-12-23 20:56:56 +0100

    ajustes de bbdd

--- src/main/java/arlenyAres/FutCat_Api/security/config/SecurityConfig.java
+++ src/main/java/arlenyAres/FutCat_Api/security/config/SecurityConfig.java
@@ -21,11 +21,11 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/api-docs/**").permitAll()
+                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/api-docs/**", "/api/auth/register", "/api/auth/login").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(withDefaults()) // Autenticación básica
-                .csrf(csrf -> csrf.disable()); // Deshabilitar CSRF (opcional, dependiendo de tu caso de uso)
+                .csrf(csrf -> csrf.disable()); // Deshabilitar CSRF
 
         return http.build();
     }
